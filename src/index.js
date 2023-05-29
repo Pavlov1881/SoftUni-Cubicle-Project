acab3827be3ab49125839b031c5fef247a6bb1af
Anatoli Pavlov <18app81@gmail.com>
2023-05-29 15:26:27 +0300

    Add static assets and resolving

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const path = require('path');
 const app = express();
-
 const port = 5000;
+
+// Express config
+app.use(express.static(path.resolve(__dirname, 'public'))); // добаване на MIDDLEWARE -> използвай директорията 'public' за всички статични файлове
 
 // handlebars confing
 app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
+extname: 'hbs',
 }));
 
 app.set('view engine', 'hbs');
-app.set('views', 'src/views'); // допълнителна настройка, за да може express да намери папката
+app.set('views', 'src/views'); // допълнителна настройка, за да може express-handlebars да намери папката
 
 // routes
 app.get('/', (req, res) => {
