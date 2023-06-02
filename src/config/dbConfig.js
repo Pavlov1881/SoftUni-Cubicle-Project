a5f537fa6d4f26fe1b879694fcfd762753aac58d
Anatoli Pavlov <18app81@gmail.com>
2023-06-02 18:09:53 +0300

    Add and config Mongoose DB

---
+++ src/config/dbConfig.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const uri = 'mongodb://localhost:27017/cubicle-may-2023';
+
+async function dbConnect() {
+    await mongoose.connect(uri);
+}
+
+module.exports = dbConnect;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
 const routes = require('./routes');
+const dbConnect = require('./config/dbConfig');
 
 const app = express();
 
 const PORT = 5000;
+
+dbConnect()
+    .then(() => console.log('DB connected successfuly'))
+    .catch(err => {
+        console.log('DB error', err)
+    });
 
 expressConfig(app);
 handlebarsConfig(app);
