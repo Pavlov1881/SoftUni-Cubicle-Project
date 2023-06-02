13764b1af4831117aa8f7fb831cbff9c41e2c67d
Anatoli Pavlov <18app81@gmail.com>
2023-06-02 18:56:49 +0300

    Update db config

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-// const uri = 'mongodb://127.0.0.1:27017/cubicle-may-2023';
 
 async function dbConnect() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/cubicle-may-2023');
+    await mongoose.connect('mongodb://127.0.0.1:27017/cubicle');
 }
 
-dbConnect();
+
 
 module.exports = dbConnect;
