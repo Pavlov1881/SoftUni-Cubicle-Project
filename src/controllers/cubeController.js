cd43cf338aaf58dd6a9892093bb6bcbcdd39cd4a
Anatoli Pavlov <18app81@gmail.com>
2023-06-08 19:52:20 +0300

    Add accessories functionality

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const cubeManager = require('../managers/cubeManager');
+const accessoryManager = require('../managers/accessoryManager');
 
 router.get('/create', (req, res) => {
     res.render('create');
@@ -25,13 +26,32 @@ router.post('/create', async (req, res) => {
 });
 
 router.get('/:cubeId/details', async (req, res) => {
-    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+    const cube = await cubeManager.getOneWithAccessories(req.params.cubeId).lean();
 
     if (!cube) {
         return res.redirect('/404');
     }
-    
+
     res.render('details', { cube });
+});
+
+router.get('/:cubeId/attach-accessory', async (req, res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+    const accessories = await accessoryManager.getOthers(cube.accessories).lean();
+
+    const hasAccessories = accessories.length > 0;
+
+    res.render('accessory/attach', { cube, accessories, hasAccessories });
+});
+
+router.post('/:cubeId/attach-accessory', async (req, res) => {
+    const { accessory: accessoryId } = req.body;
+    console.log(accessoryId);
+    const cubeId = req.params.cubeId;
+
+    await cubeManager.attachAccessory(cubeId, accessoryId);
+    
+    res.redirect(`/cubes/${cubeId}/details`);
 });
 
 module.exports = router;
