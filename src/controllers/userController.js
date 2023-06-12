3be419089cbca8ce40940e4692846b17bb341db7
Anatoli Pavlov <18app81@gmail.com>
2023-06-12 12:14:04 +0300

    Add login page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const userManager = require('../managers/userManager');
 
 router.get('/register', (req, res) => {
-    res.render('user/register')
+    res.render('users/register')
 });
 
 router.post('/register', async (req, res) => {
@@ -11,8 +11,13 @@ router.post('/register', async (req, res) => {
 
     await userManager.register({ username, password, repeatPassword })
 
-
     res.redirect('/users/login');
 });
+
+router.get('/login', (req, res) => {
+    res.render('users/login')
+});
+
+
 
 module.exports = router;
